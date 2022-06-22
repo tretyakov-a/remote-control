7b6054a723f2b825a52fba2df87cc1290e99c359
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-22 13:11:29 +0300

    feat: add draw & print_scrn commands

--- index.ts
+++ index.ts
@@ -33,6 +33,70 @@ function moveMouse(distance: number = 0, direction: DIRECTION): void {
   }
 }
 
+function drawCircle(radius: number = 0) {
+  const { x, y } = robot.getMousePos();
+
+  const twoPI = 2 * Math.PI;
+  const oneDeg = twoPI / 360; 
+
+  robot.mouseToggle('down');
+  for (let angle = 0; angle <= twoPI; angle += oneDeg) {
+    const dx = radius * Math.sin(angle);
+    const dy = radius * Math.cos(angle);
+    robot.moveMouse(x + dx, y + radius - dy);
+  }
+  robot.mouseToggle('up');
+}
+
+function drawRectangle(width: number = 0, height: number = 0) {
+  let { x, y } = robot.getMousePos();
+
+  const STEP = 1;
+  robot.mouseToggle('down');
+
+  for (let i = x; i <= x + width; i += STEP) {
+    robot.moveMouse(i, y);
+  }
+  x += width;
+
+  for (let j = y; j <= y + height; j += STEP) {
+    robot.moveMouse(x, j);
+  }
+  y += height;
+
+  for (let i = x; i >= x - width; i -= STEP) {
+    robot.moveMouse(i, y);
+  }
+  x -= width;
+
+  for (let j = y; j >= y - height; j -= STEP) {
+    robot.moveMouse(x, j);
+  }
+  
+  robot.mouseToggle('up');
+}
+
+function sendPrintScreen(ws: WebSocket) {
+  const { x, y } = robot.getMousePos();
+  const size = 200;
+  const halfSize = size / 2;
+  const img = robot.screen.capture(x - halfSize, y - halfSize, size, size);
+
+  let jimg = new Jimp(size, size);
+  for (let x = 0; x < size; x += 1) {
+    for (let y = 0; y < size; y += 1) {
+      const hex = img.colorAt(x, y);
+      const num = parseInt(hex + "ff", 16)
+      jimg.setPixelColor(num, x, y);
+    }
+  }
+
+  jimg.getBase64Async(Jimp.MIME_PNG)
+    .then((buffer) => {
+
+      ws.send(`prnt_scrn ${buffer.toString().split(',')[1]}`);
+    })
+}
 
 type Command = {
   name: string,
@@ -63,6 +127,10 @@ wss.on('connection', (ws) => {
       case 'mouse_down': moveMouse(param1, D.DOWN); break;
       case 'mouse_left': moveMouse(param1, D.LEFT); break;
       case 'mouse_right': moveMouse(param1, D.RIGHT); break;
+      case 'draw_circle': drawCircle(param1); break;
+      case 'draw_rectangle': drawRectangle(param1, param2); break;
+      case 'draw_square': drawRectangle(param1, param1); break;
+      case 'prnt_scrn': sendPrintScreen(ws); break;
     }
 
   });
